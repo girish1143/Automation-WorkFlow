5851b830423c807f5e10cbe20300a788597b75fb
Developer <dev@example.com>
2026-01-28 05:46:35 +0530

    fix login form validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-"use client";
+"use server";
 
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
-const Page = async () => {
+export default async function Page() {
   await requireAuth();
+  
+  const data = await caller.getUsers();
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       Protected server-side page
+      <div>{JSON.stringify(data)}</div>
     </div>
   );
 }
-
-export default Page;
